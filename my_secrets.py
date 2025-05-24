046c6e6e808f5fae431a75055609a8f4b571a0fe
MariaMannan <malik.maria6200@gmail.com>
2025-05-24 23:46:56 +0500

    chatbot with chainlit and openai sdk

---
+++ my_secrets.py
@@ -0,0 +1,19 @@
+import os
+import rich
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+gemini_base_url = os.getenv("GEMINI_BASE_URL")
+gemini_api_model = os.getenv("GEMINI_MODEL")
+
+if not gemini_api_key or not gemini_base_url or not gemini_api_model:
+    print("Please set environment variables")
+    exit(1)
+
+class Secrets:
+    def __init__(self):
+        self.gemini_api_key = gemini_api_key
+        self.gemini_base_url = gemini_base_url
+        self.gemini_api_model = gemini_api_model
